a5a7c8dfecc0c895fbc820b74ed003d004dad679
ramblekb <ramblekb@gmail.com>
2021-08-07 20:41:35 -0400

    portfolio layout

---
+++ src/components/ContactUs.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to reach out. Let's connect and grow our network!  
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget" style={{ textAlign: 'center' }}>
+              <h4>KipBradleyTunes@gmail.com</h4>
+              <h4>423.667.3911</h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/components/Resume.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+
+export default  class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+         </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Work</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.work && resumeData.work.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         <div className="row skill">
+
+            <div className="three columns header-col">
+               <h1><span>Skills</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+               <p>
+               {/* {resumeData.skillsDescription} */}
+               </p>
+
+ <ul className="skills">
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Material UI</li>
+              <li>Bootstrap</li>
+              <li>API Routes</li>
+              <li>MongoDB</li>
+
+                
+   					</ul> 
+             {/* <div className="bars"> */}
+   				   {/* <ul className="skills">
+                {
+                  resumeData.skills && resumeData.skills.map((item) => {
+                    return(
+                      <li>
+                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                      </span><em>{item.skillname}</em>
+                      </li>
+                    )
+                  })
+                }
+   					</ul> */}
+
+   				{/* </div> */}
+
+   			</div>
+
+         </div>
+
+      </section>
+    );
+  }
+}
