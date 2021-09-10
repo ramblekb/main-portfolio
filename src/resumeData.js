7a0f38f71801e66eb2db2a6d05aee346f2643582
ramblekb <ramblekb@gmail.com>
2021-09-10 00:07:39 -0500

    fixing portfolio links

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,121 +1,127 @@
 let resumeData = {
-    "imagebaseurl":"https://ramblekb.github.io/KipSmithPortfolio/",
-    "name": "Kip Smith",
-    "role": "Web Development",
-    "linkedinId": " linkedin.com/in/kipbradleysmith",
-    "emailAddress": " kipbradleytunes@gmail.com ",
-    "phoneNumber": " 423.667.3911",
-    "roleDescription": " I enjoy creating and managing projects that challange my expertise in front and backend web development. My passion to learn about new technologies and create content drives me as a developer. For fun I like be outdoors with my dog.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/kipbradleysmith/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/ramblekb",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"email",
-          "url":"ramblekb@gmail.com",
-          "className":"fa fa-home"
-        }
-      ],
-    "aboutme":"Current operations coordinator with a background in audio engineering and a newfound focus on web development. Three years of experience in website design, adaptation and maintenance. Seeking to bring my knowledge of front and back-end web development to advance and optimize your applications performance. Possessing a Bachelor of Liberal Arts from the University of Mississippi and a Full-Stack Web Development Certificate from Vanderbilt University. Creative by nature with a fearless work ethic and born team leader.",
-    "address":"Nashville, TN",
-    "email":"kipbradleytunes@gmail.com",
-    "phone":"423.667.3911",
-    "website":"kipbradleytunes@gmail.com // 423.667.3911",
-    "education":[
-      {
-        "UniversityName":"Vanderbilt University",
-        "specialization":"Full Stack Web Development",
-        "MonthOfPassing":"November",
-        "YearOfPassing":"2019",
-        "Achievements":"Official Certificate of Completion"
-      },
-      {
-        "UniversityName":"University of Mississippi",
-        "specialization":"Bachelor of Liberal Arts",
-        "MonthOfPassing":"May",
-        "YearOfPassing":"2010",
-        "Achievements":"Deans List"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Vintage King",
-        "specialization":"Operation Coordinator Assistant",
-        "MonthOfLeaving":"August",
-        "YearOfLeaving":"2021",
-        "Achievements":"Management & Shipping"
-      },
-      {
-        "CompanyName":"Vanderbilt Athletics",
-        "specialization":"Production Assistant",
-        "MonthOfLeaving":"Started 2021",
-        "YearOfLeaving":"Active",
-        "Achievements":"Event Broadcasting"
-      }
-    ],
-    "skillsDescription":"Web Development",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"React.js"
-      },
-      {
-        "skillname": "Node.js"
-      },
-      {
-        "skillname":"Redux"
-      },
-      {
-        "skillname":"API Routing"
-      },
-      {
-        "skillname":"MongoDB"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"Google Books Reading List",
-        "description":"Google Books API Search",
-        "imgurl":"images/portfolio/reading-list.png"
-      },
-      {
-        "name":"Shopping List",
-        "description":"MERN Stack App with CRUD Operations",
-        "imgurl":"images/portfolio/reading-list.png"
-      },
-      {
-        "name":"Bertie's Pets",
-        "description":"MERN Stack App with Status Update Button Feature",  
-        "imgurl":"images/portfolio/berties-pets.png"
-      },
-      {
-        "name":"Bottle Nose Records",
-        "description":"Full Stack record trading application with user login authentication and profile",
-        "imgurl":"images/portfolio/bottle-nose.png"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"Kip is a hard worker with a fantastic attitude. Kind, thoughtful and a great fit for any team",
-        "name":"Douglas Anton -Director of Operations at Vintage King"
-      },
-      {
-        "description":"Musicians make great coders",
-        "name":"Vishu Guntupalli -Software Engineer at Live Person"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://ramblekb.github.io/KipSmithPortfolio/",
+  "name": "Kip Smith",
+  "role": "Web Development",
+  "linkedinId": " linkedin.com/in/kipbradleysmith",
+  "emailAddress": " kipbradleytunes@gmail.com ",
+  "phoneNumber": " 423.667.3911",
+  "roleDescription": " I enjoy creating and managing projects that challange my expertise in front and backend web development. My passion to learn about new technologies and create content drives me as a developer. For fun I like be outdoors with my dog.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/kipbradleysmith/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/ramblekb",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "email",
+      "url": "ramblekb@gmail.com",
+      "className": "fa fa-home"
+    }
+  ],
+  "aboutme": "Current operations coordinator with a background in audio engineering and a newfound focus on web development. Three years of experience in website design, adaptation and maintenance. Seeking to bring my knowledge of front and back-end web development to advance and optimize your applications performance. Possessing a Bachelor of Liberal Arts from the University of Mississippi and a Full-Stack Web Development Certificate from Vanderbilt University. Creative by nature with a fearless work ethic and born team leader.",
+  "address": "Nashville, TN",
+  "email": "kipbradleytunes@gmail.com",
+  "phone": "423.667.3911",
+  "website": "kipbradleytunes@gmail.com // 423.667.3911",
+  "education": [
+    {
+      "UniversityName": "Vanderbilt University",
+      "specialization": "Full Stack Web Development",
+      "MonthOfPassing": "November",
+      "YearOfPassing": "2019",
+      "Achievements": "Official Certificate of Completion"
+    },
+    {
+      "UniversityName": "University of Mississippi",
+      "specialization": "Bachelor of Liberal Arts",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2010",
+      "Achievements": "Deans List"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Vintage King",
+      "specialization": "Operation Coordinator Assistant",
+      "MonthOfLeaving": "August",
+      "YearOfLeaving": "2021",
+      "Achievements": "Management & Shipping"
+    },
+    {
+      "CompanyName": "Vanderbilt Athletics",
+      "specialization": "Production Assistant",
+      "MonthOfLeaving": "Started 2021",
+      "YearOfLeaving": "Active",
+      "Achievements": "Event Broadcasting"
+    }
+  ],
+  "skillsDescription": "Web Development",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "React.js"
+    },
+    {
+      "skillname": "Node.js"
+    },
+    {
+      "skillname": "Redux"
+    },
+    {
+      "skillname": "API Routing"
+    },
+    {
+      "skillname": "MongoDB"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Google Books Reading List",
+      "description": "Google Books API Search",
+      // "imgurl": "images/portfolio/reading-list.png"
+      "imgurl": "https://readingapi.herokuapp.com/"
+
+    },
+    {
+      "name": "Reading List",
+      "description": "MERN Stack App with CRUD Operations",
+      "imgurl": "https://quiet-meadow-46686.herokuapp.com/"
+    },
+    {
+      "name": "Coders Hangman",
+      "description": "Inspired by the original hangman game",
+      // "imgurl": "images/portfolio/berties-pets.png"
+      "imgurl": "https://ramblekb.github.io/hangman/"
+
+    },
+    {
+      "name": "Bottle Nose Records",
+      "description": "Full Stack record trading application with user login authentication and profile",
+      // "imgurl": "images/portfolio/bottle-nose.png"
+      "imgurl": "https://new-girl.herokuapp.com/"
+
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "Kip is a hard worker with a fantastic attitude. Kind, thoughtful and a great fit for any team",
+      "name": "Douglas Anton -Director of Operations at Vintage King"
+    },
+    {
+      "description": "Musicians make great coders",
+      "name": "Vishu Guntupalli -Software Engineer at Live Person"
+    }
+  ]
+}
+
+export default resumeData
